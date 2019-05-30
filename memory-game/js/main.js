7939ef4ec827c1c16b2d0b18e1a89a31d2607b03
Lori Pinkham <loripinkham@Loris-MacBook-Pro.local>
2019-05-30 09:34:00 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,21 @@
 //console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
-
-
-var cardOne = cards[0];
-var cardTwo = cards[2];
 var cardsInPlay =[];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 
-if ((cardsInPlay.length === 2) && (cardsInPlay[0] === cardsInPlay[1])) {
-alert("You found a match");
-}
-else {
-	alert("Sorry, try again");
-}
+function flipCard(cardId) {
+	cardsInPlay.push(cardId);
+	console.log("User flipped " + cardId)
+};
+
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+};
+
+flipCard(cards[3]);
